585b73d01d801bf6846b1abb4630db014e3edd0c
mmeitneriumm <mmeitneriumm@gmail.com>
2023-05-14 11:15:12 +0300

    latest version

--- src/js/components/menu/index.js
+++ src/js/components/menu/index.js
@@ -3,9 +3,11 @@
 export default {
 	init() {
 		const burger = document.querySelector('.burger');
+		const menu = document.querySelector('.menu');
 
 		burger.addEventListener('click', () => {
 			burger.classList.toggle('active');
+			menu.classList.toggle('active');
 		});
 	}
 }
